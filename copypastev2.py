5e90751b22fedf92a43219063d25975615c3ad76
Berk <161392045+berkborazan@users.noreply.github.com>
2024-10-29 14:17:03 +0200

    Add files via upload

---
+++ copypastev2.py
@@ -0,0 +1,28 @@
+import pyperclip
+import keyboard
+
+def modify_clipboard():
+    previous_text = ""
+    while True:
+        # Get the current text from the clipboard
+        if keyboard.is_pressed("ctrl+c"):
+            current_text = pyperclip.paste()
+
+            # Check if the clipboard text has changed
+            if current_text != previous_text:
+                # Replace line breaks with spaces
+                modified_text = current_text.replace('\n', ' ').replace('\r', ' ').strip().capitalize()
+                # Update the clipboard if the text has been modified
+                pyperclip.copy(modified_text)
+                print(f"Modified Clipboard Text: {modified_text}")
+                previous_text = current_text
+
+if __name__ == "__main__":
+    print("Clipboard Modifier is running. Press 'Ctrl+C' to stop.")
+    try:
+        # Start modifying the clipboard
+        modify_clipboard()
+    except KeyboardInterrupt:
+        print("\nClipboard Modifier stopped.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
